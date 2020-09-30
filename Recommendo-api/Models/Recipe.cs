170be24a72212312118b494651b162b2ec205e22
Shane Leusink <Shane.Leusink@oak.com>
2020-09-30 07:44:07 +0100

    Add project files.

---
+++ Recommendo-api/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recommendo_api.Models
+{
+    public class Recipe
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> Method { get; set; }
+    }
+}
